7e3ae8b091c1e99093de7092e0f4a5d8bd080693
donniegene <da34773@ravens.racc.edu>
2019-04-23 13:35:38 -0400

    Code from 2019-04-18

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 // inside out project STEP-502
 
-$("body").prepend('<div class=text-white style=position:fixed;top:0;bottom:0;width:100%;z-index:99999999;padding-top:12%;background-color:rgba(87,87,87,.95);" id="cover-page"><div id="cover-page-con" class="container text-center"style="opacity:100%;"><img alt="Don Allen"class="img-fluid rounded-circle"src=http://via.placeholder.com/150x150><h1>Don Allen</h1><p class=lead>...inspired innovative technology leader & follower.<p class=lead style=font-size:80%><i class="fa-1x fa-cog fa-spin fas"></i> we are preparing your experiance now...</div></div>');
+$("body").prepend('<div class=text-white style=position:fixed;top:0;bottom:0;width:100%;z-index:99999999;padding-top:12%;background-color:rgba(87,87,87,.95);" id="cover-page"><div id="cover-page-con" class="container text-center"style="opacity:100%;"><img alt="Brian Savage"class="img-fluid rounded-circle"src=http://via.placeholder.com/150x150><h1>Brian Savage</h1><p class=lead>...inspired innovative technology leader & follower.<p class=lead style=font-size:80%><i class="fa-1x fa-cog fa-spin fas"></i> we are preparing your experiance now...</div></div>');
 
 var parentPageID = 303;
 
@@ -9,7 +9,7 @@ window.onload = getContent();
 function getContent() {
     $.ajax({
         method: 'GET',
-        url: 'https://me.donaldallenjr.com/wp-json/wp/v2/pages/?parent=' + parentPageID + '&order=asc&orderby=menu_order',
+        url: 'https://me.inside-out-project.com/wp-json/wp/v2/pages/?parent=' + parentPageID + '&order=asc&orderby=menu_order',
         dataType: 'json',
         success: function (data) {
             if (data.length > 0) {
